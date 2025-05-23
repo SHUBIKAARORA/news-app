221bb18b783f91e6ea05a31d4aeb6c384a47beb4
SHUBIKAARORA <shubikaarora2004@gmail.com>
2025-05-23 12:00:53 +0530

    Updates

---
+++ precache-manifest.55188134c27b481446ee6995ade229d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a70772b68788e876d4ff0e6e82db9a",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "198623ee7ddcedd5c0e5",
+    "url": "/news-app/static/css/main.5366b050.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/news-app/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/news-app/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "198623ee7ddcedd5c0e5",
+    "url": "/news-app/static/js/main.26620df8.chunk.js"
+  },
+  {
+    "revision": "62ed8abddae0485b68d0",
+    "url": "/news-app/static/js/runtime~main.9aecd5f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SHUBIKAARORA/news-app/precache-manifest.fbea47a8fcd239e3e9281bda6c59cae8.js"
+  "/news-app/precache-manifest.55188134c27b481446ee6995ade229d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SHUBIKAARORA/news-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
