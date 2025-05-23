0d65bda5ca9efe65f8db5af63b3a01e89b656da8
SHUBIKAARORA <shubikaarora2004@gmail.com>
2025-05-23 11:55:01 +0530

    Updates

---
+++ precache-manifest.fbea47a8fcd239e3e9281bda6c59cae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea465029ee11de9eecb34454dd84c65",
+    "url": "/SHUBIKAARORA/news-app/index.html"
+  },
+  {
+    "revision": "198623ee7ddcedd5c0e5",
+    "url": "/SHUBIKAARORA/news-app/static/css/main.5366b050.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/SHUBIKAARORA/news-app/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/SHUBIKAARORA/news-app/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "198623ee7ddcedd5c0e5",
+    "url": "/SHUBIKAARORA/news-app/static/js/main.26620df8.chunk.js"
+  },
+  {
+    "revision": "5cef1515f0f1f44c2352",
+    "url": "/SHUBIKAARORA/news-app/static/js/runtime~main.f7dd8a46.js"
+  }
+]);
